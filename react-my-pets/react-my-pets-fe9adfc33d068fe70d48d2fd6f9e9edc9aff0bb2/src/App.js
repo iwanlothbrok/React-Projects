6478b8b880c2d8c095c1b697b2ee6e68fe95aa9a
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-06-20 09:44:03 +0300

    React my-pets project added, fetched by Ivaylo Papazov

---
+++ react-my-pets/react-my-pets-fe9adfc33d068fe70d48d2fd6f9e9edc9aff0bb2/src/App.js
@@ -0,0 +1,29 @@
+import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Categories from './components/Categories/Categories';
+import PetDetails from './components/PetDetails/PetDetails';
+import DemoPage from './components/Demo';
+import DemoFuncPage from './components/DemoFunc';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <Header />
+
+        <Switch>
+          <Route path="/" exact component={Categories} />
+          <Route path="/categories/:category" component={Categories} />
+          <Route path="/pets/details/:petId" component={PetDetails} />
+          <Route path="/demo" component={DemoPage} />
+          <Route path="/demo-func" component={DemoFuncPage} />
+        </Switch>
+        
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
