cec56f529969fd6e4e586c20fe611f625d6302b2
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-06-20 17:56:35 +0300

    app js was changed

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,15 +1,37 @@
 import PokemonList from "./components/PokemonList";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bukbasulr"]);
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPage, setPage] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [nextPage, setNextPage] = useState();
+  const [previousPage, setPreviousPage] = useState();
+  const [loading, setLoading] = useState(true);
 
-  axios.get();
 
+
+  useEffect(() => {
+    let cancel
+    setLoading(true);
+    axios.get(currentPage, {
+
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+
+      setLoading(false);
+      setNextPage(res.data.next);
+      setPreviousPage(res.data.previous);
+      setPokemon(res.data.results.map(p => p.name));
+    });
+
+    return () => cancel.cancel();
+  }, [currentPage])
+
+  if (loading) return 'Loading...';
   return (
-    <PokemonList pokemon={pokemon}/>
+    <PokemonList pokemon={pokemon} />
   );
 }
 
--- pokemon/src/components/PokemonList.js
+++ pokemon/src/components/PokemonList.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export default function ({ pokemon }) {
+    console.log(pokemon.name);
     return (
         <div>
-            {pokemon.map(p => (
-                <div key={p}>{p}</div>
-            ))}
+            {/* {pokemon.map(p => (
+                <div key={p.name}>{p.name}</div>
+            ))} */}
+            {pokemon.name}
         </div>
     )
 }
