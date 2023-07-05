a31d6399fae6b1559805008ff9768fe0a6d97e38
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-05 13:53:31 +0300

    items component, api controller and model were created

---
+++ ASP_REACT/RankingApp/RankingApp/ClientApp/src/components/RankedItems.js
@@ -0,0 +1,23 @@
+﻿import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+export default function RankedItems() {
+
+
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+
+        axios.get(`item/${dataType}`)
+            .then(res => { res.json() })
+            .then(res => { setItems(res) }), []);
+
+    return (
+        <main>
+            {
+                (items != null) ? (items.map((it) => <h3>{it.title}</h3>)) : <h2>Loading....</h2>
+            }
+        </main>
+    )
+}
---
+++ ASP_REACT/RankingApp/RankingApp/Controllers/ItemController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RankingApp.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ItemController : ControllerBase
+	{
+
+
+
+
+	}
+}
