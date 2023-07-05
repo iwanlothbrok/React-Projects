f8c384ccde8781a17755d42ca6c6eb5e178327cb
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-05 17:57:55 +0300

    css for photos was added

--- ASP_REACT/RankingApp/RankingApp/ClientApp/src/components/RankedItems.js
+++ ASP_REACT/RankingApp/RankingApp/ClientApp/src/components/RankedItems.js
@@ -1,4 +1,6 @@
 ﻿import React, { useEffect, useState } from 'react'
+import MovieImageArr from './MovieImageArr.js'
+
 
 export default function RankedItems() {
 
@@ -15,9 +17,16 @@ export default function RankedItems() {
 
     return (
         <main>
-            {
-                (items.length > 0) ? (items.map((it) => <h3>{it.title}</h3>)) : <h2>Loading...</h2>
-            }
-        </main>
+            <div className="items-not-ranked">
+                {
+                    (items.length > 0) ? items.map((it) =>
+                        //                              checking from the array with the images with find 
+                        <div className="unranked-cell">
+                            < img key={`item-${it.id}`} src={MovieImageArr.find(im => im.id === it.id)?.image} />
+                        </div>
+                    ) : <h2>Loading...</h2>
+                }
+            </div>
+        </main >
     )
 }
