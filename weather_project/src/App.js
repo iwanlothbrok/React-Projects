c9445360b7edd4d4610762ba09db64340945352b
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-14 11:43:30 +0300

    initial commit

---
+++ weather_project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
