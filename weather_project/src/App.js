8b6bf7a8e5fbf7730d454544b2128406934c9703
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-17 14:20:39 +0300

    Weather API is working properly

--- weather_project/src/App.js
+++ weather_project/src/App.js
@@ -1,38 +1,60 @@
 import logo from './logo.svg';
 import react, { useState } from 'react'
-
+import axios from 'axios';
 
 function App() {
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
   const [date, sateDate] = useState({});
   const [location, setLocation] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
+  const searchingLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((res) => {
+        sateDate(res.data);
+        console.log(res.data);
+      }).catch((e) => {
+        setLocation('');
+      })
+    }
 
+  }
 
   return (
     <div className="app">
+      <div className='search'>
+        <input
+          value={location}
+          onChange={e => setLocation(e.target.value)}
+          onKeyUp={searchingLocation}
+          placeholder='Set Location'
+          type='text'>
+        </input>
+      </div>
       <div className='container'>
         <div className='top'>
           <div className='location'>
-            <p>Plovdiv</p>
+            <p>{date.name}</p>
           </div>
           <div className='temp'>
-            <h1>40</h1>
+            {date.main ? <h1>{date.main.temp}°</h1> : null}
           </div>
           <div className='description'>
-            <p>Hot</p>
+            {date.weather ? <h1>{date.weather[0].main}</h1> : null}
           </div>
         </div>
         <div className='bottom'>
-          <div className='fells'>
-            <p className='bold'>60</p>
+          <div className='feels'>
+            {date.main ? <p className='bold'>{date.main.feels_like}°F</p> : null}
+            <p>Feels Like</p>
           </div>
           <div className='humidity'>
-            <p className='bold'>120</p>
+            {date.main ? <p className='bold'>{date.main.humidity}%</p> : null}
+            <p>Humidity</p>
           </div>
           <div className='wind'>
-            <p className='bold'>120</p>
+            {date.wind ? <p className='bold'>{date.wind.speed} MPH</p> : null}
+            <p>Wind Speed</p>
           </div>
         </div>
       </div>
