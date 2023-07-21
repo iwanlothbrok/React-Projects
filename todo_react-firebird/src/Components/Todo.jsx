7d86290dcc7990759f131c8ca44ba3001a06e3f5
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-21 14:52:28 +0300

    The whole Todo component was changed, and the old way of adding todos was removed

--- todo_react-firebird/src/Components/Todo.jsx
+++ todo_react-firebird/src/Components/Todo.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { FaRegTrashAlt } from 'react-icons/fa'
+const style = {
+    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+    liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+    row: `flex`,
+    text: `ml-2 cursor-pointer`,
+    textComplete: `ml-2 cursor-pointer line-through`,
+    button: `cursor-pointer flex items-center`,
+};
 
-const Todo = ({ todos }) => {
-    let counter = 0;
+const Todo = ({ todo }) => {
 
     return (
-        todos.map(t => <li key={counter += 1}>{t} </li>)
+        <li className={style.li}>
+            <div className={style.row}>
+                <input type="checkbox" className="w-checkbox-lg h-checkbox-lg border-2 border-black rounded-md"></input>
+                <p className={style.text}>{todo}</p>
+            </div>
+            <button>{<FaRegTrashAlt size={30} />}</button>
+        </li>
+
     )
 }
 
