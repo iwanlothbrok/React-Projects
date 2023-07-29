25649d4dc22c178994c8569528326ae747156ee9
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-29 10:11:57 +0300

    02 done

--- products_project/tt-main/TT.Lib/Entities/Property.cs
+++ products_project/tt-main/TT.Lib/Entities/Property.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TT.Lib.Entities
 {
     public class Property : BaseName
     {
-        public int ParentId { get; set; }   
+        public int ParentId { get; set; }
+
+        /// <summary>
+        /// if it's necessary
+        /// </summary>
+        /// [Required]
+        [StringLength(50)]
+        public string Type { get; set; }
     }
 }
