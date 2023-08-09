6ea8abec7cbdbea9041bb1dcafe31306e15de8c7
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-08-09 13:23:27 +0300

    fetching information from API and getting first only the name, then the whole json

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  // setting the styles like object 
+  const h1Style = {
+    'textAlign': 'center',
+    'color': 'blue'
+  }
+
+  const [info, setInfo] = useState('')
+
+  async function fetchInfo() {
+    try {
+      const response = await axios.get('https://randomuser.me/api/');
+
+      let { title, first, last } = response.data.results[0].name;
+
+      //setInfo(`${title}: ${first} ${last}`)
+      setInfo(JSON.stringify(response.data));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchInfo();
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={h1Style}>Que pasa?</h1>
+      <h1>{info}</h1>
     </div>
   );
 }
